e32082456ba025739e50839414c272a9e52c5b36
ElGoreLoco <esegoreloco@gmail.com>
2016-05-07 20:20:54 +0200

    Change background

--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@ local circle = love.graphics.newImage("assets/circle.png")
 local cross = love.graphics.newImage("assets/cross.png")
 
 function love.load()
+    love.graphics.setBackgroundColor(255, 255, 255)
 end
 function love.draw()
     love.graphics.draw(circle, 0, 0)
