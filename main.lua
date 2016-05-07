3d57bda007981e6d004a41e990709aecdf657572
ElGoreLoco <esegoreloco@gmail.com>
2016-05-07 19:53:41 +0200

    Export images and test them

--- main.lua
+++ main.lua
@@ -1,6 +1,10 @@
+local circle = love.graphics.newImage("assets/circle.png")
+local cross = love.graphics.newImage("assets/cross.png")
+
 function love.load()
 end
-function love.run()
+function love.draw()
+    love.graphics.draw(circle, 0, 0)
 end
 function love.update()
 end
